2fb2eb5329a6645e7796778fac938051b36973b9
shubhamsingh <singhshubhamkumar66@gmail.com>
2024-10-03 07:25:19 -0700

    Shubham:- initial commit

---
+++ src/test/java/com/libraryManagement/org/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryManagement.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
